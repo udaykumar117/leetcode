36b578d44d3b539a709d66ef233a451acb8fdce0
udaykumar117 <72568252+udaykumar117@users.noreply.github.com>
2021-06-16 10:08:45 +0530

    backtracking

--- problems/src/backtracking/Combinations.java
+++ problems/src/backtracking/Combinations.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Given two integers n and k, return all possible combinations of
+ *  two integers n and k, return all possible combinations of
  * k numbers out of 1 ... n.
  *
  * <p>For example, If n = 4 and k = 2, a solution is:
